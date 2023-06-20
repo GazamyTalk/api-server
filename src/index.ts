7aaab3564f8a4f278526ef460b2ab7a7b0f3d14b
JuHwiSang <juhs2005@gmail.com>
2023-06-20 20:09:33 +0900

    fix: add query log

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     console.log('req.session:', req.session);
     console.log('req.headers:', req.headers);
     console.log('req.body:', req.body);
+    console.log('req.query:', req.query);
     next();
 })
 
